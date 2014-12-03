c795d8393a6c09f15cf9593a7d5a05d7a6727196
Sheina Sim <ssim8@hawaii.edu>
2014-12-02 16:27:11 -1000

    Update fasta_length.py

--- fasta_length.py
+++ fasta_length.py
@@ -5,7 +5,8 @@ from src.fasta_reader import read_fasta
 
 def main():
     if len(sys.argv) < 2:
-        print("Usage: fasta_length.py <bin_size> <fasta1> <fasta2> <fasta3> ...")
+        sys.stderr.write("Usage: fasta_length.py <bin_size> <fasta1> <fasta2> <fasta3> ...")
+        sys.exit()
         
     bin_size = int.(sys.argv[1])
     
@@ -13,12 +14,17 @@ def main():
     All_taxa_read_lengths = {}
     
     for arg in sys.argv[2:]:
-        Read_length_list = []
+        #Take name of argument (all words before .fa or .fasta) and turn that into a variable
+        taxa = arg.strip().split(.)[1]
+        #Create list for read lengths associated with taxa
+        taxa_read_length_list = []
         with open(arg, 'r') as fasta:
             seqs = read_fasta(fasta)
             for seq in seqs:
-                Read_length = len(seq.bases)
-                Read_length_list.append(Read_length)
+                read_length = len(seq.bases)
+                taxa_read_length_list.append(read_length)
+        #Add key and list to dictionary
+        All_taxa_read_lengths["taxa"] = taxa_read_length_list
 
 ##################################3
 
@@ -26,4 +32,4 @@ if __name__ == "__main__":
     main()
 
 #Make and plot histogram with plt.hist()
-plst.hist(Read_length_list, bins=bin_size)
+plst.hist(All_taxa_read_lengths, bins=bin_size)
