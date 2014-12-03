8e49600d0969795a83852241310b85f52067060c
Sheina Sim <ssim8@hawaii.edu>
2014-12-02 15:53:26 -1000

    Update fasta_length.py

--- fasta_length.py
+++ fasta_length.py
@@ -4,28 +4,26 @@ import matplotlib.pyplot as plt
 from src.fasta_reader import read_fasta
 
 def main():
-    if len(sys.argv) == 1:
+    if len(sys.argv) < 2:
         print("Usage: fasta_length.py <bin_size> <fasta1> <fasta2> <fasta3> ...")
         
-    for arg in sys.argv[1]:
-        bin_size = sys.argv[1]
-        
+    bin_size = int.(sys.argv[1])
+    
+    #Create dictionary
+    All_taxa_read_lengths = {}
+    
     for arg in sys.argv[2:]:
+        Read_length_list = []
         with open(arg, 'r') as fasta:
             seqs = read_fasta(fasta)
             for seq in seqs:
-                pass
+                Read_length = len(seq.bases)
+                Read_length_list.append(Read_length)
 
 ##################################3
 
 if __name__ == "__main__":
     main()
-    
-#Create list that holds values to be binned with plt.hist()
-Read_length_list = []
-
-#Add reads to list
-Read_length_list.append(Read_lengths)
 
 #Make and plot histogram with plt.hist()
 plst.hist(Read_length_list, bins=bin_size)
